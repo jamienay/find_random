6597c8ec7d3973a7b2983f138923fe515a41a448
Jamie Nay <jamie.nay@gmail.com>
2010-01-19 07:28:27 -0800

    Added closing brace to AppModel class definition; added custom find method

--- app_model.php
+++ app_model.php
@@ -1,6 +1,22 @@
 <?php
 class AppModel extends Model {
     
+    function find($type, $options = array()) {
+        $method = null;
+        if (is_string($type)) {
+            $method = sprintf('__find%s', Inflector::camelize($type));
+        }
+        
+        if ($method && method_exists($this, $method)) {
+            $results = $this->{$method}($options);
+        } else {
+            $args = func_get_args();
+            $results = call_user_func_array(array('parent', 'find'), $args);
+        }
+
+        return $results;
+    }
+      
     /**
      * __findRandom()
      *
@@ -78,4 +94,5 @@ class AppModel extends Model {
             return $this->find('all', $findOptions);
         }
     }
+}
 ?>
